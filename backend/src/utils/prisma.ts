0d77009e955dcf0a4c55a99c5ead38c400082f2e
Oksana <Ksn.al@icloud.com>
2026-05-20 04:39:28 +0300

    fix: add url to datasource for prisma

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,10 +1,9 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 import dotenv from 'dotenv'
 
-dotenv.config()
+dotenv.config();
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
