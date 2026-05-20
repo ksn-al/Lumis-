9091876f49d8b83aa2e3ffd8d0331218cb66b6d6
Oksana <Ksn.al@icloud.com>
2026-05-20 19:47:40 +0300

    fix: добавлен CORS для фронта на onrender (redeploy)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,11 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "http://192.168.10.49:3000",
-    "https://lumis-app.onrender.com"
-  ],
+  origin: true,
   credentials: true
 }));
 
